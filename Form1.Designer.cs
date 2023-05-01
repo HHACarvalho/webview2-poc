d1c86930c92af4d5e3d9af818ae2a5f323a2f9b0
Hugo Carvalho <hhac@protonmail.com>
2023-05-01 18:44:26 +0100

    Test commit

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,8 +30,8 @@
         {
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(900, 600);
+            this.Text = "WebView2 Application";
         }
 
         #endregion
