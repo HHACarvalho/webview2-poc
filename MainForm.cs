5355a0ec19f860807312069c0a9cab529e39812f
Hugo Carvalho <hhac@protonmail.com>
2023-05-03 16:56:51 +0100

    Added the option to browse the web

--- MainForm.cs
+++ MainForm.cs
@@ -16,5 +16,28 @@ namespace webview2_poc
         {
             InitializeComponent();
         }
+
+        private void buttonSubmit_Click(object sender, EventArgs e)
+        {
+            var rawUrl = textUrl.Text;
+            Uri uri = null;
+
+            if (Uri.IsWellFormedUriString(rawUrl, UriKind.Absolute))
+            {
+                uri = new Uri(rawUrl);
+            }
+            else if (!rawUrl.Contains(" ") && rawUrl.Contains("."))
+            {
+                uri = new Uri("http://" + rawUrl);
+            }
+            else
+            {
+                uri = new Uri("https://google.com/search?q=" +
+                    String.Join("+", Uri.EscapeDataString(rawUrl).Split(new string[] { "%20" }, StringSplitOptions.RemoveEmptyEntries)));
+            }
+
+            textUrl.Text = uri.ToString();
+            webViewWindow.Source = uri;
+        }
     }
 }
