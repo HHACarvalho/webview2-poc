1b6d5a26d7315a9fd8e810b80b9b66ce26131a64
Hugo Carvalho <hhac@protonmail.com>
2023-05-13 15:20:15 +0100

    Renamed controls and their methods for better usability

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -29,28 +29,28 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
-            this.mainWV = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.mainWebView = new Microsoft.Web.WebView2.WinForms.WebView2();
             this.buttonGo = new System.Windows.Forms.Button();
             this.textUrl = new System.Windows.Forms.TextBox();
             this.buttonScreenshot = new System.Windows.Forms.Button();
-            this.secondaryWV = new Microsoft.Web.WebView2.WinForms.WebView2();
-            this.textUrlSec = new System.Windows.Forms.TextBox();
-            this.buttonScreenshotSec = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.mainWV)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.secondaryWV)).BeginInit();
+            this.offscreenWebView = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.textUrlOffscreen = new System.Windows.Forms.TextBox();
+            this.buttonScreenshotOffscreen = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.mainWebView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.offscreenWebView)).BeginInit();
             this.SuspendLayout();
             // 
-            // mainWV
+            // mainWebView
             // 
-            this.mainWV.AllowExternalDrop = true;
-            this.mainWV.CreationProperties = null;
-            this.mainWV.DefaultBackgroundColor = System.Drawing.Color.White;
-            this.mainWV.Location = new System.Drawing.Point(12, 41);
-            this.mainWV.Name = "mainWV";
-            this.mainWV.Size = new System.Drawing.Size(1000, 523);
-            this.mainWV.Source = new System.Uri("https://www.google.com/", System.UriKind.Absolute);
-            this.mainWV.TabIndex = 0;
-            this.mainWV.ZoomFactor = 1D;
+            this.mainWebView.AllowExternalDrop = true;
+            this.mainWebView.CreationProperties = null;
+            this.mainWebView.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.mainWebView.Location = new System.Drawing.Point(12, 41);
+            this.mainWebView.Name = "mainWebView";
+            this.mainWebView.Size = new System.Drawing.Size(1000, 523);
+            this.mainWebView.Source = new System.Uri("https://www.google.com/", System.UriKind.Absolute);
+            this.mainWebView.TabIndex = 0;
+            this.mainWebView.ZoomFactor = 1D;
             // 
             // buttonGo
             // 
@@ -60,7 +60,7 @@
             this.buttonGo.TabIndex = 1;
             this.buttonGo.Text = "Go";
             this.buttonGo.UseVisualStyleBackColor = true;
-            this.buttonGo.Click += new System.EventHandler(this.buttonSubmit_Click);
+            this.buttonGo.Click += new System.EventHandler(this.buttonGo_Click);
             // 
             // textUrl
             // 
@@ -81,54 +81,55 @@
             this.buttonScreenshot.UseVisualStyleBackColor = true;
             this.buttonScreenshot.Click += new System.EventHandler(this.buttonScreenshot_Click);
             // 
-            // secondaryWV
+            // offscreenWebView
             // 
-            this.secondaryWV.AllowExternalDrop = true;
-            this.secondaryWV.CreationProperties = null;
-            this.secondaryWV.DefaultBackgroundColor = System.Drawing.Color.White;
-            this.secondaryWV.Location = new System.Drawing.Point(12, 700);
-            this.secondaryWV.Name = "secondaryWV";
-            this.secondaryWV.Size = new System.Drawing.Size(1000, 523);
-            this.secondaryWV.Source = new System.Uri("https://www.youtube.com/", System.UriKind.Absolute);
-            this.secondaryWV.TabIndex = 4;
-            this.secondaryWV.TabStop = false;
-            this.secondaryWV.ZoomFactor = 1D;
+            this.offscreenWebView.AllowExternalDrop = true;
+            this.offscreenWebView.CreationProperties = null;
+            this.offscreenWebView.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.offscreenWebView.Location = new System.Drawing.Point(12, 700);
+            this.offscreenWebView.Name = "offscreenWebView";
+            this.offscreenWebView.Size = new System.Drawing.Size(1000, 523);
+            this.offscreenWebView.Source = new System.Uri("https://www.youtube.com/", System.UriKind.Absolute);
+            this.offscreenWebView.TabIndex = 4;
+            this.offscreenWebView.TabStop = false;
+            this.offscreenWebView.ZoomFactor = 1D;
+            this.offscreenWebView.NavigationCompleted += new System.EventHandler<Microsoft.Web.WebView2.Core.CoreWebView2NavigationCompletedEventArgs>(this.requestScreenshot);
             // 
-            // textUrlSec
+            // textUrlOffscreen
             // 
-            this.textUrlSec.Location = new System.Drawing.Point(12, 573);
-            this.textUrlSec.Name = "textUrlSec";
-            this.textUrlSec.Size = new System.Drawing.Size(918, 20);
-            this.textUrlSec.TabIndex = 5;
-            this.textUrlSec.Text = "https://www.youtube.com/";
+            this.textUrlOffscreen.Location = new System.Drawing.Point(12, 573);
+            this.textUrlOffscreen.Name = "textUrlOffscreen";
+            this.textUrlOffscreen.Size = new System.Drawing.Size(918, 20);
+            this.textUrlOffscreen.TabIndex = 5;
+            this.textUrlOffscreen.Text = "https://www.youtube.com/";
             // 
-            // buttonScreenshotSec
+            // buttonScreenshotOffscreen
             // 
-            this.buttonScreenshotSec.Location = new System.Drawing.Point(936, 571);
-            this.buttonScreenshotSec.Name = "buttonScreenshotSec";
-            this.buttonScreenshotSec.Size = new System.Drawing.Size(75, 23);
-            this.buttonScreenshotSec.TabIndex = 6;
-            this.buttonScreenshotSec.Text = "Screenshot";
-            this.buttonScreenshotSec.UseVisualStyleBackColor = true;
-            this.buttonScreenshotSec.Click += new System.EventHandler(this.buttonScreenshotSec_Click);
+            this.buttonScreenshotOffscreen.Location = new System.Drawing.Point(936, 571);
+            this.buttonScreenshotOffscreen.Name = "buttonScreenshotOffscreen";
+            this.buttonScreenshotOffscreen.Size = new System.Drawing.Size(75, 23);
+            this.buttonScreenshotOffscreen.TabIndex = 6;
+            this.buttonScreenshotOffscreen.Text = "Screenshot";
+            this.buttonScreenshotOffscreen.UseVisualStyleBackColor = true;
+            this.buttonScreenshotOffscreen.Click += new System.EventHandler(this.buttonScreenshotOffscreen_Click);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1024, 606);
-            this.Controls.Add(this.buttonScreenshotSec);
-            this.Controls.Add(this.textUrlSec);
-            this.Controls.Add(this.secondaryWV);
+            this.Controls.Add(this.buttonScreenshotOffscreen);
+            this.Controls.Add(this.textUrlOffscreen);
+            this.Controls.Add(this.offscreenWebView);
             this.Controls.Add(this.buttonScreenshot);
             this.Controls.Add(this.textUrl);
             this.Controls.Add(this.buttonGo);
-            this.Controls.Add(this.mainWV);
+            this.Controls.Add(this.mainWebView);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MainForm";
             this.Text = "WebView2 Application";
-            ((System.ComponentModel.ISupportInitialize)(this.mainWV)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.secondaryWV)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mainWebView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.offscreenWebView)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -136,12 +137,12 @@
 
         #endregion
 
-        private Microsoft.Web.WebView2.WinForms.WebView2 mainWV;
+        private Microsoft.Web.WebView2.WinForms.WebView2 mainWebView;
         private System.Windows.Forms.Button buttonGo;
         private System.Windows.Forms.TextBox textUrl;
         private System.Windows.Forms.Button buttonScreenshot;
-        private Microsoft.Web.WebView2.WinForms.WebView2 secondaryWV;
-        private System.Windows.Forms.TextBox textUrlSec;
-        private System.Windows.Forms.Button buttonScreenshotSec;
+        private Microsoft.Web.WebView2.WinForms.WebView2 offscreenWebView;
+        private System.Windows.Forms.TextBox textUrlOffscreen;
+        private System.Windows.Forms.Button buttonScreenshotOffscreen;
     }
 }
