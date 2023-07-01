f2d3302b36da701a73195b391b76eb8227d7f064
Hugo Carvalho <hhac@protonmail.com>
2023-07-01 17:56:21 +0100

    Created a service class on the PowerPoint project

---
+++ PowerPoint/PowerPointService.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Office.Core;
+using Shared;
+using System.IO;
+
+namespace PowerPoint
+{
+    internal class PowerPointService : SharedService
+    {
+        public override void AddImage(string imagePath)
+        {
+            var currentSlide = Globals.ThisAddIn.Application.ActiveWindow.View.Slide;
+            currentSlide.Shapes.AddPicture(imagePath, MsoTriState.msoTrue, MsoTriState.msoTrue, 0, 0);
+            File.Delete(imagePath);
+        }
+    }
+}
